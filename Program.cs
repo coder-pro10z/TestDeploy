2b439a530e9e4bf6fc34d2f5e5f5796176006143
Praveen Kashyap <85899936+coder-pro10z@users.noreply.github.com>
2026-02-13 23:51:13 +0530

    add builder

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+//Create Builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Add basic services
